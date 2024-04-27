8001c57dcc3d0633130d28e2d404bc182ab330e6
H2Owater425 <h2o@h2owr.xyz>
2024-04-27 08:42:18 +0000

    fix: Ensure not to panic

--- source/routes/search/+page.server.ts
+++ source/routes/search/+page.server.ts
@@ -11,76 +11,85 @@ export function load(event: LoadEvent): Promise<{
 	exams: Exam[];
 } {
 	if(event['url']['searchParams'].has('query')) {
-		const rawQuery: string = base64Decode(event['url']['searchParams'].get('query') as string);
+		try {
+			const rawQuery: string = base64Decode(event['url']['searchParams'].get('query') as string);
 
-		let query: string = '';
-		let queryIndex: number = 0;
-		let currentIndex: number = 0;
-		let nextIndex: number = rawQuery.indexOf(',');
+			let query: string = '';
+			let queryIndex: number = 0;
+			let currentIndex: number = 0;
+			let nextIndex: number = rawQuery.indexOf(',');
 
-		while(nextIndex !== -1 && queryIndex !== 5) {
-			const rawQueryValue: number = Number(rawQuery.slice(currentIndex, nextIndex));
-			const queryName: string = QUERY_NAMES[queryIndex];
+			while(nextIndex !== -1 && queryIndex !== 5) {
+				const rawQueryValue: number = Number(rawQuery.slice(currentIndex, nextIndex));
+				const queryName: string = QUERY_NAMES[queryIndex];
 
-			if(queryIndex < 2) {
-				for(const queryValue of getBitsPositions(rawQueryValue)) {
-					query += '&' + queryName + '=' + queryValue;
+				if(queryIndex < 2) {
+					for(const queryValue of getBitsPositions(rawQueryValue)) {
+						query += '&' + queryName + '=' + queryValue;
+					}
+				} else if(queryIndex === 2) {
+					for(const queryValue of getBitsPositions(rawQueryValue)) {
+						query += '&' + queryName + '=' + MONTH_NAMES[queryValue - 1];
+					}
+				} else {
+					query += '&' + queryName + '=' + rawQueryValue;
 				}
-			} else if(queryIndex === 2) {
-				for(const queryValue of getBitsPositions(rawQueryValue)) {
-					query += '&' + queryName + '=' + MONTH_NAMES[queryValue - 1];
-				}
-			} else {
-				query += '&' + queryName + '=' + rawQueryValue;
+
+				currentIndex = nextIndex + 1;
+				nextIndex = rawQuery.indexOf(',', currentIndex);
+				queryIndex++;
+			}
+			
+			if(queryIndex === 5 && currentIndex !== rawQuery['length']) {
+				query += '&term=' + encodeURIComponent(rawQuery.slice(currentIndex));
 			}
 
-			currentIndex = nextIndex + 1;
-			nextIndex = rawQuery.indexOf(',', currentIndex);
-			queryIndex++;
-		}
+			query = query.slice(1);
+
+			//query += '&page[size]=7000';
 		
-		if(queryIndex === 5 && currentIndex !== rawQuery['length']) {
-			query += '&term=' + encodeURIComponent(rawQuery.slice(currentIndex));
-		}
+			return event.fetch('/api/exams?' + query)
+			.then(function (response: Response): Promise<{
+				status: 'success';
+				data: Exam[];
+			}> {
+				if(response['status'] === 200) {
+					return response.json();
+				} else {
+					throw null;
+				}
+			})
+			.then(function (responseJson: {
+				status: 'success';
+				data: Exam[];
+			}): {
+				query: string;
+				exams: Exam[];
+			} {
+				return {
+					query: query,
+					exams: responseJson['data']
+				};
+			})
+			.catch(function (error: unknown): {
+				query: string;
+				exams: Exam[];
+			} {
+				console.error(error);
 
-		query = query.slice(1);
-
-		//query += '&page[size]=7000';
-	
-		return event.fetch('/api/exams?' + query)
-		.then(function (response: Response): Promise<{
-			status: 'success';
-			data: Exam[];
-		}> {
-			if(response['status'] === 200) {
-				return response.json();
-			} else {
-				throw null;
-			}
-		})
-		.then(function (responseJson: {
-			status: 'success';
-			data: Exam[];
-		}): {
-			query: string;
-			exams: Exam[];
-		} {
-			return {
-				query: query,
-				exams: responseJson['data']
-			};
-		})
-		.catch(function (error: unknown): {
-			query: string;
-			exams: Exam[];
-		} {
+				return {
+					query: '',
+					exams: []
+				};
+			});
+		} catch(error: unknown) {
 			console.error(error);
 
 			return {
 				query: '',
 				exams: []
 			};
-		});
+		}
 	} else {
 		console.error(new Error('Query[\'query\'] must be exist'));
 
