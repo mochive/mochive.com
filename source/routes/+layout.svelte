3dd812c9f2c63d5b8a7ca59b11c471e3f99831df
H2Owater425 <h2o@h2owr.xyz>
2024-05-09 09:11:54 +0900

    feat: Add cloudflare web analytics with partytown

--- source/routes/+layout.svelte
+++ source/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang='ts'>
 	import 'pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css';
+	import { partytownSnippet } from '@builder.io/partytown/integration';
 </script>
 
 <svelte:window on:beforeunload={function () {
@@ -8,13 +9,18 @@
 	return;
 }} />
 
+<svelte:head>
+  {@html '<script>window[\'partytown\']={forward:[\'dataLayer.push\',\'gtag\']};' + partytownSnippet() + '</script>'}
+	{@html '<script defer type=\'text/partytown\' src=\'https://static.cloudflareinsights.com/beacon.min.js\' data-cf-beacon=\'{"token":"38c6f17baf9048be87e5d48ecbae00f4"}\'></script>'}
+</svelte:head>
+
 <slot />
 
 <style>
 	:global(*) {
 		margin: 0;
 		padding: 0;
-		font-family: "Pretendard JP Variable", "Pretendard JP", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Hiragino Sans", "Apple SD Gothic Neo", Meiryo, "Noto Sans JP", "Noto Sans KR", "Malgun Gothic", Osaka, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+		font-family: 'Pretendard JP Variable', 'Pretendard JP', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Hiragino Sans', 'Apple SD Gothic Neo', Meiryo, 'Noto Sans JP', 'Noto Sans KR', 'Malgun Gothic', Osaka, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
 		outline: none;
 		user-select: none;
 		word-break: keep-all;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import svg from '@poppanator/sveltekit-svg';
+import { partytownVite } from '@builder.io/partytown/utils';
+import { join } from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit(), svg({
+	plugins: [sveltekit(), partytownVite({
+		dest: join(__dirname, 'static', '~partytown')
+	}), svg({
 		svgoOptions: {
 			plugins: [{
 				name: 'preset-default',
