41791f954c61024064d86d8a2b3298238850d5bf
H2Owater425 <h2o@h2owr.xyz>
2024-05-09 11:41:38 +0900

    fix: Remove asynchronous script load

--- source/routes/+layout.svelte
+++ source/routes/+layout.svelte
@@ -10,8 +10,8 @@
 }} />
 
 <svelte:head>
-  {@html '<script>window[\'dataLayer\']=[];window[\'partytown\']={forward:[\'dataLayer.push\']};' + partytownSnippet() + '</script>'}
-	{@html '<script async type=\'text/partytown\' src=\'https://www.googletagmanager.com/gtag/js?id=G-WZQZBE6Z4T\'>dataLayer.push(\'js\',new Date(),\'config\',\'G-WZQZBE6Z4T\');</script>'}
+  {@html '<script>partytown={forward:[\'dataLayer.push\']};' + partytownSnippet() + '</script>'}
+	{@html '<script type=\'text/partytown\' src=\'https://www.googletagmanager.com/gtag/js?id=G-WZQZBE6Z4T\'>window[\'dataLayer\']=[];dataLayer.push([\'js\',new Date()],[\'config\', \'YOUR-ID-HERE\']);</script>'}
 </svelte:head>
 
 <slot />
