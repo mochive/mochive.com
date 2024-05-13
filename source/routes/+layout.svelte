4b1113b698fe0bcd06d29e6a99193ed90294fcb9
H2Owater425 <h2o@h2owr.xyz>
2024-05-13 15:34:07 +0900

    feat: Add cloudflare web analytics

--- source/routes/+layout.svelte
+++ source/routes/+layout.svelte
@@ -8,6 +8,10 @@
 	return;
 }} />
 
+<svelte:head>
+	{@html '<script defer src=\'https://static.cloudflareinsights.com/beacon.min.js\' data-cf-beacon=\'{"token": "38c6f17baf9048be87e5d48ecbae00f4"}\'></script>'}
+</svelte:head>
+
 <slot />
 
 <style>
