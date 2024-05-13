87ce31cf1083f4f0a8aa60e961a9860b595d2cf3
H2Owater425 <h2o@h2owr.xyz>
2024-05-13 06:44:25 +0000

    fix: Use string for data
    feat: Add fallback

--- source/routes/api/coffee/+server.ts
+++ source/routes/api/coffee/+server.ts
@@ -2,13 +2,13 @@ import { JsendStatus } from '$lib/constant';
 import { getJsendResponse } from '$lib/utility';
 
 export function GET(): Response {
-	return getJsendResponse({
-		title: 'I\'m sorry, but this server is powered by Teapot™'
-	}, JsendStatus['FAIL'], 418);
+	return getJsendResponse('I\'m sorry, but this server is powered by Teapot™', JsendStatus['FAIL'], 418);
 }
 
 export function POST(): Response {
-	return getJsendResponse({
-		title: 'I\'m sorry, but this server is powered by Teapot™'
-	}, JsendStatus['FAIL'], 418);
+	return getJsendResponse('I\'m sorry, but this server is powered by Teapot™', JsendStatus['FAIL'], 418);
+}
+
+export function fallback(): Response {
+	return getJsendResponse('Method must be valid', JsendStatus['FAIL'], 405);
 }
