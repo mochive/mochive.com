4d2dda84a257167457de983edcfd8801c78095f8
H2Owater425 <h2o@h2owr.xyz>
2024-05-19 22:10:24 +0900

    fix: Fix canonical url

--- source/routes/.well-known/security.txt/+server.ts
+++ source/routes/.well-known/security.txt/+server.ts
@@ -1,6 +1,5 @@
 export function GET(): Response {
-	const body: string = 'Contact: mailto:me@kangmin.kim\nContact: mailto:support@mochive.com\nExpires: ' + ((new Date()).getFullYear() + 1) + '-01-01T00:00:00.000Z\nPreferred-Languages: ko, en\nCanonical: https://beta.mochive.com/.well-known/security.txt';
-
+	const body: string = 'Contact: mailto:me@kangmin.kim\nContact: mailto:support@mochive.com\nExpires: ' + ((new Date()).getFullYear() + 1) + '-01-01T00:00:00.000Z\nPreferred-Languages: ko, en\nCanonical: https://mochive.com/.well-known/security.txt';
 
 	return new Response(body, {
 		headers: {
